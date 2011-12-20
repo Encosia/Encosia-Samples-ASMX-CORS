421c8d4e37e29d655b3481a44c21bbf2c768e105
Dave Ward <dave@encosia.com>
2011-12-20 13:12:38 -0500

    Initial

---
+++ WebService.asmx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Encosia.Samples.ASMX_CORS {
+  [System.Web.Script.Services.ScriptService]
+  public class WebService : System.Web.Services.WebService {
+    [WebMethod]
+    public string HelloWorld() {
+      return "Hello World";
+    }
+  }
+}
